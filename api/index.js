319af1a597f87b1b6263a914a258c7652a03857c
Nuru Hussen <weddihaji@gmail.com>
2024-05-05 17:56:39 +0300

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+dotenv.config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Myblog")
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+///uploding image
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },  
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running on 5000.");
+});
